79fde10873a39551fb6758099a52b2bb96203351
Stuart Harrison <pezholio@gmail.com>
2020-07-03 15:22:07 +0100

    Put templates relative to the app

--- app/config/settings/base.py
+++ app/config/settings/base.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
 
-PROJECT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+PROJECT_DIR = os.path.dirname(os.path.dirname((os.path.abspath(__file__))))
 BASE_DIR = os.path.dirname(PROJECT_DIR)
 
 
@@ -69,7 +69,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
-            os.path.join(PROJECT_DIR, 'templates'),
+            os.path.join(BASE_DIR, 'templates'),
         ],
         'APP_DIRS': True,
         'OPTIONS': {
