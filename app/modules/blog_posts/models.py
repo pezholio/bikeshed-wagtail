5812ee06e93af77ad8747fc4eef203a5af657313
Stuart Harrison <pezholio@gmail.com>
2020-07-07 14:30:15 +0100

    Add date to blog posts

--- app/modules/blog_posts/models.py
+++ app/modules/blog_posts/models.py
@@ -1,8 +1,16 @@
+from wagtail.admin.edit_handlers import FieldPanel
 from modules.core.models.abstract import BasePage, BaseIndexPage
+from wagtail.admin.edit_handlers import StreamFieldPanel
 
 class BlogPost(BasePage):
     parent_page_types = ['BlogPostIndexPage']
     subpage_types = []
+
+    content_panels = [
+        FieldPanel('title'),
+        FieldPanel('first_published_at'),
+        StreamFieldPanel('body')
+    ]
 
 class BlogPostIndexPage(BaseIndexPage):
     subpage_types = ['BlogPost']
